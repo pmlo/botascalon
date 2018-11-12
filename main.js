95185d95f999013bef14bfc022084b618d6947fd
ZaynMc <theobourrier@outlook.fr>
2018-11-12 19:37:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -99,8 +99,7 @@ bot.on("message", async message => {
 bot.on('guildMemberAdd', member => 
        
        
-    let channelWelcome = member.guild.channels.find(`name`, `👐bienvenue`);
-       channelWelcome.sendMessage(`Bienvenue ${member} , avant de te divertir sur  ASCALON CUP je t'invite à lire le règlement . Have Fun:tada::hugging: !`
+       //channelWelcome.sendMessage(`Bienvenue ${member} , avant de te divertir sur  ASCALON CUP je t'invite à lire le règlement . Have Fun:tada::hugging: !`
 
    // if (member.guild.id !== serverStats.guildID) return;
 
