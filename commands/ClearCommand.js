777dd6ade89626b474b5d48b88c6d0d87d510b46
ZaynMc <theobourrier@outlook.fr>
2018-11-15 21:39:46 +0100

    Update ClearCommand.js

--- commands/ClearCommand.js
+++ commands/ClearCommand.js
@@ -2,9 +2,26 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, ops) => {
 
-    message.delete();
     
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.delete();
+    let messagecount = parseInt(args[0]) || 1;
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+    
+    
+    
+   /** if(!message.member.hasPermission("MANAGE_MESSAGES")) {
         message.delete();
         return;
     }
@@ -17,10 +34,27 @@ module.exports.run = async (bot, message, args, ops) => {
 
     message.channel.bulkDelete(args[0]).then(() => {
         message.channel.send(`Cleared ${args[0]} messages.`).then(message => message.delete(5000));
-    });
+    });**/
 
 }
 
 module.exports.help = {
     name: "clear"
   }
+
+//let messagecount = parseInt(args[1]) || 1;
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (m.author.id == bot.user.id) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
