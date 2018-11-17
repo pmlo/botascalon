de06ff283d2e85be7f319acaf7c6c657f4675346
ZaynMc <theobourrier@outlook.fr>
2018-11-17 19:11:35 +0100

    Update LeaveCommand.js

--- commands/LeaveCommand.js
+++ commands/LeaveCommand.js
@@ -11,6 +11,9 @@ module.exports.run = async (bot, message, args, ops) => {
 
    //LEAVE CHANNEL
    message.guild.me.voiceChannel.leave();
+   
+   const logChannel = member.guild.channels.find('name', 'history-bot');
+    logChannel.send(`[LOG] BOT QUIT CHANNEL ${message.channel.name}`);
 
    message.channel.send("Le bot quitte le channel...");
 }
