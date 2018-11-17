b6191329fa10c43a1268c3759aa31019f41948c9
ZaynMc <theobourrier@outlook.fr>
2018-11-17 18:54:09 +0100

    Update BanCommand.js
    change ligne 69

--- commands/BanCommand.js
+++ commands/BanCommand.js
@@ -66,7 +66,7 @@ module.exports.run = async (bot, message, args, ops) => {
     const joinChannel = member.guild.channels.find('name', 'history-bot');
     joinChannel.send(`[LOG] USER ${rUser} BANNED by ${message.author} for reason ${rreason}`);
 
-    let banchannel = message.guild.channels.find(`name`, "history-bot");
+    let banchannel = message.guild.channels.find('name', 'history-bot');
     let banchannelsanction = message.guild.channels.find(`id`, "509790654113644571");
     if(!banchannel) return;
     
