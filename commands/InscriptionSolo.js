b2497be1819bcbd30fe2282b7a7a930f7b2619f8
ZaynMc <theobourrier@outlook.fr>
2018-11-23 20:17:20 +0100

    Update InscriptionSolo.js

--- commands/InscriptionSolo.js
+++ commands/InscriptionSolo.js
@@ -16,6 +16,11 @@ module.exports.run = async (bot, message, args, ops) => {
       message.channel.send(`<@${message.author.id}> Vous êtes déjà inscrit`);
       return;
     }
+    if(bot.numberisinscriptionsolo == 2) {
+        message.channel.send(`<@${message.author.id}> Le tournoi est plein.`);
+        return;
+    }
+    
     bot.numberisinscriptionsolo+=1;
     const joinChannel = message.member.guild.channels.find('name', '🚻equipe-inscrite');
     joinChannel.send(`Le joueur ${messages} (<@${message.author.id}>) vient de s'inscrire pour le tournoi **ASCALON SOLO**`);
