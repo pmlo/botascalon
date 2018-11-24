b91d7cbb57e9c1a577895e1d3081561adc03efca
ZaynMc <theobourrier@outlook.fr>
2018-11-24 22:18:38 +0100

    Update

--- commands/SoloSetTournoi.js
+++ commands/SoloSetTournoi.js
@@ -18,10 +18,10 @@ module.exports.run = async (bot, message, args, ops) => {
     bot.channels.get(channelTeam4).setName('Poule 4');
 
     const logChannel = message.member.guild.channels.find('name', 'history-bot');
-    logChannel.send(`[LOG] EQUIPE 1 TO POULE 1 CHANGED ${message.author}`);
-    logChannel.send(`[LOG] EQUIPE 2 TO POULE 2 CHANGED ${message.author}`);
-    logChannel.send(`[LOG] EQUIPE 3 TO POULE 3 CHANGED ${message.author}`);
-    logChannel.send(`[LOG] EQUIPE 4 TO POULE 4 CHANGED ${message.author}`);
+    logChannel.send(`[LOG] EQUIPE 1 TO POULE 1 CHANGED`);// ${message.author}`);
+    logChannel.send(`[LOG] EQUIPE 2 TO POULE 2 CHANGED`);// ${message.author}`);
+    logChannel.send(`[LOG] EQUIPE 3 TO POULE 3 CHANGED`);// ${message.author}`);
+    logChannel.send(`[LOG] EQUIPE 4 TO POULE 4 CHANGED`);// ${message.author}`);
 
     let Solosize = args[0];
 
