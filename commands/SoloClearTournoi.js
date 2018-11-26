463d0d7dc619c5eb4fa1b67ff41cb9b0b7024178
ZaynMc <theobourrier@outlook.fr>
2018-11-26 18:51:44 +0100

    Update

--- commands/SoloClearTournoi.js
+++ commands/SoloClearTournoi.js
@@ -10,13 +10,13 @@ module.exports.run = async (bot, message, args, ops) => {
     let channelTeam1 = "501124673354858516"; let channelTeam2 = "502944251026472970"; 
     let channelTeam3 = "502945786028490753"; let channelTeam4 = "501124944885710859"; 
     let inscriptionsolo = "511210802712412170";
+
+    bot.channels.get(inscriptionsolo).setName('🌀inscription-solo-off');
     
     bot.channels.get(channelTeam1).setName('Equipe 1 -'); bot.channels.get(channelTeam2).setName('Equipe 2 -'); 
     bot.channels.get(channelTeam3).setName('Equipe 3 -'); bot.channels.get(channelTeam4).setName('Equipe 4 -'); 
     
     ops.soloTournoi.clear();
-
-    bot.channels.get(inscriptionsolo).setName('🌀inscription-solo-off');
 
     ops.isONSOLO = false;
 
