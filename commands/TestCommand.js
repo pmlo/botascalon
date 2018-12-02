5c925456fff28db5b9ddafd8839682b6c3679010
ZaynMc <theobourrier@outlook.fr>
2018-12-02 12:52:53 +0100

    Update

--- commands/TestCommand.js
+++ commands/TestCommand.js
@@ -3,9 +3,22 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, ops) => {
 
-    if(!message.member.voiceChannel) return;
+   // if(!message.member.voiceChannel) return;
 
+   let muterole = message.guild.roles.find(`name`, "Rythm");
 
+   try{
+
+    message.guild.channels.forEach(async (channel, id) => {
+      await channel.overwritePermissions(muterole, {
+        SEND_MESSAGES: false,
+        ADD_REACTIONS: false,
+        
+      });
+    });
+  }catch(e){
+    console.log(e.stack);
+  }
 
    //if(message.guild.me.voiceChannel) return message.channel.send('Désolée mais le bot est déjà dans votre channel.');
 
