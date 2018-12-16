469c15d7f97908da40642115fd5b0c050622b5c7
ZaynMc <theobourrier@outlook.fr>
2018-12-16 22:34:27 +0100

    update

--- commands/history/SoloInscription.js
+++ commands/history/SoloInscription.js
@@ -5,32 +5,25 @@ module.exports.run = async (bot, message, args, ops) => {
 
     message.delete();
 
-    //IF NOT CHANNEL ON
-    if(message.channel.name == config.name_channel_inscription_solo_off) return message.channel.send(`<@${message.author.id}> L'inscription pour le tournoi **ASCALON SOLO** est fermer. `).then(message => message.delete(5000));
+    if(message.channel.name == "🌀inscription-solo-off") return message.channel.send(`<@${message.author.id}> L'inscription pour le tournoi **ASCALON SOLO** est fermer. `).then(message => message.delete(5000));
 
     let arg = message.content.split(" ").slice(1);
     let messages = arg.join(" ");
 
-    if(ops.soloTournoi.has(`${messages}`)) {
+    if(ops.soloTournoi.has(`${messages}`) || ops.soloTournoi.has(`<@${message.author.name}>`)) {
       message.channel.send(`<@${message.author.name}> Vous êtes déjà inscrit`);
       return;
     }
 
-    if(ops.soloTournoi.has(`<@${message.author.name}>`)) {
-      message.channel.send(`<@${message.author.id}> Vous êtes déjà inscrit`);
-      return;
-    }
-
-
-    //TOURNOI FULL
-    if(ops.soloTournoi.size >= bot.numermaxinscriptionsolo) {
+    if(ops.soloTournoi.size >= ops.numbersolomax) {
       var embed = new Discord.RichEmbed()
     .setTitle(`TOURNOI SOLO ASCALON`)
     .addField(`📛 Vous n'êtes pas inscrit au tournoi ASCALON SOLO ${messages} ! Celui ci est au complet 📛`, "🕛 Vous venez quand même au tournoi, des places se libéreront surêment ! 🕛")
     .setColor("0xB40404")
     .setTimestamp()
     .setFooter("TOURNOI SOLO ASCALON");
-      return;
+    message.guild.channels.find("id", "511210802712412170").sendEmbed(embed);
+    return;
     }
 
     const joinChannel = message.member.guild.channels.find('name', '🚻equipe-inscrite');
@@ -51,9 +44,7 @@ module.exports.run = async (bot, message, args, ops) => {
     message.guild.channels.find("id", "511210802712412170").sendEmbed(embed);
 
     const logChannel = message.member.guild.channels.find('name', 'history-bot');
-    logChannel.send(`[LOG] ${ops.soloTournoi.size} / ${bot.numermaxinscriptionsolo}`);
-
-    //📛📛
+    logChannel.send(`[LOG] ${ops.soloTournoi.size} / ${ops.numbersolomax}`);
 }
 
 module.exports.help = {
