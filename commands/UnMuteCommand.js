fa28630cca8c30d035e3a899ec5442cd7f61dd24
ZaynMc <theobourrier@outlook.fr>
2018-12-16 12:10:13 +0100

    Update UnMuteCommand.js

--- commands/UnMuteCommand.js
+++ commands/UnMuteCommand.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args, ops, tools) => {
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute){tools.log("CHECK COMMAND MUTE NOT PLAYER FOUND", message); return;}
 
-  if(tomute.hasPermission("MANAGE_MESSAGES")) {tools.log('CHECK COMMAND MUTE NOT PLAYER HAS PERMISSION'); return;}
+  if(tomute.hasPermission("MANAGE_MESSAGES")) {tools.log('CHECK COMMAND MUTE NOT PLAYER HAS PERMISSION', message); return;}
   let muterole = message.guild.roles.find(`name`, 'muted');
 
   if(message.member.roles.has(muterole.id)) {
