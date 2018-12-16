94faed61b5a47655dd1af1e70d8ad5769e8bb5d6
ZaynMc <theobourrier@outlook.fr>
2018-12-16 11:30:28 +0100

    update

--- main.js
+++ main.js
@@ -30,74 +30,51 @@ MANAGE_WEBHOOKS *	0x20000000	Allows management and editing of webhooks	T, V
 MANAGE_EMOJIS *	0x40000000	Allows management and editing of emojis
  */
 
+ const Discord = require("discord.js");
+ const fs = require("fs");
+ const bot = new Discord.Client({disableEveryone: true});
+ bot.commands = new Discord.Collection();
 
-const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-const tools = require('./function.js');
-bot.commands = new Discord.Collection();
-let token = process.env.token;
+ fs.readdir("./commands/", (err, files) => {
 
-//INSCRIPTION SOLO
-var numermaxinscriptionsolo = 0;
-var soloTournoi = new Map();
+   if(err) console.log(err);
 
-//INSCRIPTION DUOS
-var numermaxinscriptionduo = 0;
-var duoTournoi = new Map();
+   let jsfile = files.filter(f => f.split(".").pop() === "js")
+   if(jsfile.length <= 0){
+     console.log("Couldn't find commands.");
+     return;
+   }
 
-const serverStats = {
-    guildID: '511250353430462465',
-    totalUsersID: '511251090097045526',
-    memberCountID: '511251296054149141',
-    botCountID: '511251340119244841'
-}
+   jsfile.forEach((f, i) =>{
+     let props = require(`./commands/${f}`);
+     console.log(`${f} loaded!`);
+     bot.commands.set(props.help.name, props);
+   });
 
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-
-});
+ });
 
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+ bot.on("ready", async () => {
+   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 
-  bot.user.setActivity("Obéir à son maître", {type: "WATCHING"});
+   bot.user.setActivity("Obéir à mon maître", {type: "WATCHING"});
 
-});
+   //bot.user.setGame("on SourceCade!");
+ });
 
-bot.on("message", async message => {
+ bot.on("message", async message => {
+   if(message.author.bot) return;
+   if(message.channel.type === "dm") return;
 
-    //Return Statements
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+   let prefix = "a!";
+   let messageArray = message.content.split(" ");
+   let cmd = messageArray[0];
+   let args = messageArray.slice(1);
 
-  let ops = {
-      soloTournoi: soloTournoi,
-      duoTournoi: duoTournoi,
-  }
+   let commandfile = bot.commands.get(cmd.slice(prefix.length));
+   if(commandfile) commandfile.run(bot,message,args);
 
-  let prefix = "a!";
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args, ops, tools);
-});
+ });
 
 bot.on('guildMemberAdd', member => {
 
