2745f134f3955614b7dde4ca08dfd8ac4809a96d
ZaynMc <theobourrier@outlook.fr>
2018-12-16 21:35:32 +0100

    Update function.js

--- function.js
+++ function.js
@@ -7,7 +7,7 @@ module.exports = {
     joinChannel.send(`[LOG] ${message}`);
   },
 
-  initsolo: function(message, channel) {
+  initsolo: function(size) {
 
     let channelTeam1 = "501124673354858516";
     let channelTeam2 = "502944251026472970";
@@ -21,17 +21,19 @@ module.exports = {
       Discord.channels.get(channelTeam2).setName('Poule 2');
       Discord.channels.get(channelTeam3).setName('Poule 3');
       Discord.channels.get(channelTeam4).setName('Poule 4');
+      return;
     }
 
-    if(args[0] == "20") {
+    if(size == "20") {
         Discord.channels.get(channelTeam1).setName('Poule 1');
         Discord.channels.get(channelTeam2).setName('Poule 2');
         Discord.channels.get(channelTeam3).setName('Poule 3');
         Discord.channels.get(channelTeam4).setName('Poule 4');
         Discord.channels.get(channelTeam5).setName('Poule 5');
+        return;
     }
 
-    if(args[0] == "24") {
+    if(size == "24") {
         Discord.channels.get(channelTeam1).setName('Poule 1');
         Discord.channels.get(channelTeam2).setName('Poule 2');
         Discord.channels.get(channelTeam3).setName('Poule 3');
