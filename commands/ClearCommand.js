1586282fff67e2a546b3269c427470acbab089d8
ZaynMc <theobourrier@outlook.fr>
2019-01-01 23:32:37 +0100

    Update ClearCommand.js

--- commands/ClearCommand.js
+++ commands/ClearCommand.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, ops) => {
 
-
     message.delete();
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) {
@@ -16,11 +15,22 @@ module.exports.run = async (bot, message, args, ops) => {
         return;
     }
 
-    let messages = await message.channel.fetchMessages({
-    limit: args.amount && args.amount < 100 ? args.amount : 100
-  });
+    if (message.channel.type == 'text') {
+        message.channel.fetchMessages()
+          .then(messages => {
+            message.channel.bulkDelete(messages);
+            messagesDeleted = messages.array().length; // number of messages deleted
 
-  let clearedMessages = await message.channel.bulkDelete(messages, true);
+            // Logging the number of messages deleted on both the channel and console.
+            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
+            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+          })
+          .catch(err => {
+            console.log('Error while doing Bulk Delete');
+            console.log(err);
+          });
+      }
+    }
 
 
 
