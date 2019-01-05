616150a4100780198b4761372416f8b15bba437c
ZaynMc <theobourrier@outlook.fr>
2019-01-06 00:23:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -180,7 +180,7 @@ bot.on("message", async message => {
 
     if(message.author.bot) return;
 
-    if(message.channelType.toUpperCase() !== "text") {
+    if(message.channel.type !== "text") {
 
     //if(message.channelType.toUpperCase() == 'dm') {
 
