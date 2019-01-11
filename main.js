c38136d67c0613428bac48e35de8f544630c86e0
ZaynMc <theobourrier@outlook.fr>
2019-01-11 23:52:34 +0100

    update

--- main.js
+++ main.js
@@ -388,7 +388,7 @@ bot.on("message", async message => {
       if(noFound) {
         message.delete();
 
-        message.author.send("Bonjour !\n \nVotre message dans le channel #💀recrutement-team ne respecte pas le modèle donné \n \n Nous t'invitons à respecter le modèle ci-dessous lors de votre prochain post\n \n __Voici le formulaire à respecter pour publier son annonce : __ \n \n ```Nom de l'équipe (avec brève description mais facultatif) :\nPlateforme :\nÂge minimum (indiquer l'âge minimum si nécessaire) :\nRôles disponibles (joueur, coach, modérateur, manager,etc...) :\nObjectif (snipe, tournoi, fun, ...) :\nK/D minimum (indiquer le K/D minimum si nécessaire) :```\n \nÂ bientôt !");
+        message.author.send("Bonjour !\n \nVotre message dans le channel" + message.guild.channels.get('501869668919803904').toString(); +"ne respecte pas le modèle donné \n \n Nous t'invitons à respecter le modèle ci-dessous lors de votre prochain post\n \n __Voici le formulaire à respecter pour publier son annonce : __ \n \n ```Nom de l'équipe (avec brève description mais facultatif) :\nPlateforme :\nÂge minimum (indiquer l'âge minimum si nécessaire) :\nRôles disponibles (joueur, coach, modérateur, manager,etc...) :\nObjectif (snipe, tournoi, fun, ...) :\nK/D minimum (indiquer le K/D minimum si nécessaire) :```\n \nÂ bientôt !");
 
 
       }
