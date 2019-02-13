c6802a4a72422a2c3ccf0060dd4682a4e4069614
ZaynMc <theobourrier@outlook.fr>
2019-02-13 21:25:50 +0100

    update

--- commands/ClearCommand.js
+++ commands/ClearCommand.js
@@ -17,22 +17,10 @@ module.exports.run = async (bot, message, args, ops) => {
         return;
     }
 
-    if (message.channel.type == 'text') {
-        message.channel.fetchMessages()
-          .then(messages => {
-            message.channel.bulkDelete(messages);
-            messagesDeleted = messages.array().length; // number of messages deleted
-
-            // Logging the number of messages deleted on both the channel and console.
-            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
-            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
-          })
-          .catch(err => {
-            console.log('Error while doing Bulk Delete');
-            console.log(err);
-          });
-
-  }
+    message.channel.bulkDelete(args[0]).then(() => {
+      message.channel.send(`${args[0]} ont été clear !`).then(msg => msg.delete(50000));
+    });
+    
 }
 
 module.exports.help = {
