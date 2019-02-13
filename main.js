ed0e316aed9b7df9185bab4f8ce0189b730adccf
ZaynMc <theobourrier@outlook.fr>
2019-02-13 20:20:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -114,15 +114,6 @@ function changeColor() {
    if(message.channel.type === "dm") return;
 
     let ops = {
-      numbersolomax: numbersolomax,
-      soloTournoi: soloTournoi,
-      numersolobuildfightmax: numersolobuildfightmax,
-      soloBuildFightTournoi: soloBuildFightTournoi,
-      soloTurtleFightTournoi: soloTurtleFightTournoi,
-      numersoloturtlefightmax: numersoloturtlefightmax,
-      dmMessage: dmMessage,
-      idDm: idDm,
-      channelRec: channelRec,
     }
 
    let prefix = "a!";
@@ -135,32 +126,10 @@ function changeColor() {
 
  });
 
-
-
- /*bot.on('messageReactionAdd', (reaction, user) => {
-
-        if (user.id !== bot.user.id) {
-
-          if(reaction.message.channel.name == "🚨annonce🚨"){
-            if(reaction.emoji.name == "✅"){
-              let users = bot.users.find("username", user.username);
-              users.send("Voici le lien de l'invitation : https://discord.gg/U3K7aKB Besoin d'aide ? Une fonctionnalité du serveur qui fonctionne mal, contactez Zayn#0607");
-          }
-        }
-      }
-    });*/
-
 bot.on('guildMemberAdd', member => {
 
     const joinChannel = member.guild.channels.find('name', '👐bienvenue');
     joinChannel.send(`Bienvenue ${member.user}, avant de te divertir sur **ASCALON** je t'invite à lire le règlement . Have Fun🎉🤗 !`);
-
-    /*let embed = new Discord.RichEmbed()
-        .setColor('#33cc33')
-        .setDescription('Bienvenue ' + member.user.name + ", avant de te divertir sur ASCALON je t'invite à lire le règlement . Have Fun🎉🤗 !")
-        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-
-        joinChannel.send(embed);*/
 
     bot.channels.get(serverStats.totalUsersID).setName(`Total Users : ${member.guild.memberCount}`); // total users
     bot.channels.get(serverStats.memberCountID).setName(`Member Count : ${member.guild.members.filter(m => !m.user.bot).size}`); // total members (not inscued bot)
@@ -308,43 +277,6 @@ bot.on("message", async message => {
           console.log(e.stack);
         }
 
-        /**
-         * CREATE_INSTANT_INVITE	0x00000001	Allows creation of instant invites	T, V
-        KICK_MEMBERS *	0x00000002	Allows kicking members
-        BAN_MEMBERS *	0x00000004	Allows banning members
-        ADMINISTRATOR *	0x00000008	Allows all permissions and bypasses channel permission overwrites
-        MANAGE_CHANNELS *	0x00000010	Allows management and editing of channels	T, V
-        MANAGE_GUILD *	0x00000020	Allows management and editing of the guild
-        ADD_REACTIONS	0x00000040	Allows for the addition of reactions to messages	T
-        VIEW_AUDIT_LOG	0x00000080	Allows for viewing of audit logs
-        VIEW_CHANNEL	0x00000400	Allows guild members to view a channel, which includes reading messages in text channels	T, V
-        SEND_MESSAGES	0x00000800	Allows for sending messages in a channel	T
-        SEND_TTS_MESSAGES	0x00001000	Allows for sending of /tts messages	T
-        MANAGE_MESSAGES *	0x00002000	Allows for deletion of other users messages	T
-        EMBED_LINKS	0x00004000	Links sent by users with this permission will be auto-embedded	T
-        ATTACH_FILES	0x00008000	Allows for uploading images and files	T
-        READ_MESSAGE_HISTORY	0x00010000	Allows for reading of message history	T
-        MENTION_EVERYONE	0x00020000	Allows for using the @everyone tag to notify all users in a channel, and the @here tag to notify all online users in a channel	T
-        USE_EXTERNAL_EMOJIS	0x00040000	Allows the usage of custom emojis from other servers	T
-        CONNECT	0x00100000	Allows for joining of a voice channel	V
-        SPEAK	0x00200000	Allows for speaking in a voice channel	V
-        MUTE_MEMBERS	0x00400000	Allows for muting members in a voice channel	V
-        DEAFEN_MEMBERS	0x00800000	Allows for deafening of members in a voice channel	V
-        MOVE_MEMBERS	0x01000000	Allows for moving of members between voice channels	V
-        USE_VAD	0x02000000	Allows for using voice-activity-detection in a voice channel	V
-        PRIORITY_SPEAKER	0x00000100	Allows for using priority speaker in a voice channel	V
-        CHANGE_NICKNAME	0x04000000	Allows for modification of own nickname
-        MANAGE_NICKNAMES	0x08000000	Allows for modification of other users nicknames
-        MANAGE_ROLES *	0x10000000	Allows management and editing of roles	T, V
-        MANAGE_WEBHOOKS *	0x20000000	Allows management and editing of webhooks	T, V
-        MANAGE_EMOJIS *	0x40000000	Allows management and editing of emojis
-         */
-
-    /*  channel = await guild.channels.create(`${message.author.username}-${message.author.discriminator}`, {
-        parent: '531463499550031882',
-        topic: `?complete to close the ticket | Support for ${message.author.tag} | ID : ${message.author.id}`
-      });*/
-
       let author = message.author;
 
       const newChannel = new Discord.RichEmbed()
@@ -356,15 +288,7 @@ bot.on("message", async message => {
 
       await channel.send(newChannel);
 
-      /*const newTicket = new Discord.RichEmbed()
-      .setColor(0x36393e)
-      .setAuthor(`Hello, ${author.tag}`)
-      .setFooter('Support Ticket Created')
-
-      await author.send(newTicket);*/
-
       author.send(":flag_mf: Merci d'avoir contacté le support ! Un membre du staff va vous contactez dans les plus brefs délais \n:flag_lr: Thank you for contacting the support ! A member of the staff will contact you as soon as possible");
-    //  author.send(":flag_lr: Thank you for contacting the support ! A member of the staff will contact you as soon as possible");
 
       active.channelID = channel.id;
       active.targetID = author.id;
@@ -374,15 +298,7 @@ bot.on("message", async message => {
 
     channel = bot.channels.get(active.channelID);
 
-    /*const dm = new Discord.RichEmbed()
-    .setColor(0x36393e)
-    .setAuthor(`Thanks you, ${message.author.tag}`)
-    .setFooter(`Your message has been sent -- A staff member will be in contact soon`)
-
-    await message.author.send(dm);*/
-
     message.author.send(":flag_mf: Votre message a été envoyé \n:flag_lr: Your message has been sent");
-  //  message.author.send(":flag_lr: Your message has been sent");
 
     const embed = new Discord.RichEmbed()
     .setColor(0x36393e)
@@ -408,40 +324,21 @@ bot.on("message", async message => {
 
     if(message.content.toLowerCase() == '?complete') {
 
-        /*const complete = new Discord.RichEmbed()
-        .setColor(0x36393e)
-        .setAuthor(`Hey, ${supportUser.tag}`)
-        .setFooter('Ticket Closed')
-        .setDescription('*Your ticket has been marked as **complete**. If you wish to reopen this.')
-
-        supportUser.send(complete);*/
-
         message.channel.delete();
 
         db.delete(`support_${support.targetID}`);
         return;
           }
 
-    /*const embed = new Discord.RichEmbed()
-    .setColor(0x36393e)
-    .setAuthor(message.author.tag)
-    .setFooter(`Message Recieved -- Zayn`)
-    .setDescription(message.content)*/
-
     bot.users.get(support.targetID).send(message.content)
-    //bot.users.get(support.targetID).send(embed)
-
     message.delete();
 
-    //embed.setFooter(`Message Sent -- ${supportUser.tag}`).setDescription(message.content);
 
-    return message.channel.send(message.content); //message.channel.send(embed);
+    return message.channel.send(message.content);
   }
   });
 
   bot.login(token);
-
-  // LANCEMENT DE GAME, INSCRIPTION, (CLASSEMENT)
 
 
 
