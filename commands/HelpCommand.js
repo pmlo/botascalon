0d3ed53e12ee22279540650e08a0d3467224ba39
ZaynMc <theobourrier@outlook.fr>
2019-02-17 11:40:39 +0100

    Update HelpCommand.js

--- commands/HelpCommand.js
+++ commands/HelpCommand.js
@@ -4,6 +4,27 @@ module.exports.run = async (bot, message, args, ops) => {
 
     message.delete();
 
+    const embed = new Discord.RichEmbed()
+  .setTitle("Commands du bot")
+  .setAuthor("Zayn", "https://imgur.com/a/piqpgDh")
+
+  .setColor(0x00AE86)
+  .setFooter("Créer par Zayn", "https://imgur.com/a/piqpgDh")
+  .setThumbnail("https://imgur.com/a/Dm3s5x8")
+
+  .setTimestamp()
+  .addField("a!ban @User Raison","Cette commande permet de ban un joueur du serveur.", true)
+
+  .addField("a!kick @User", "Cette commande permet de kick un joueur du serveur.", true)
+
+  .addField("a!mute @User 1s/m/h/d", "Cette command permet de mute un joueur sur un temps défini, il est impossible de mute permanent quelqu'un.", true)
+
+  .addField("a!unmute @User", "Cette commande permet d'unmute un joueur, il est impossible d'unmute une personne non mute.")
+
+  .addBlankField(true)
+ 
+  message.channel.send({embed});
+
 }
 
 module.exports.help = {
