ef46c77b1ef45f2fda89b38c2cfcd0a7acc53b37
ZaynMc <theobourrier@outlook.fr>
2019-02-21 21:07:48 +0100

    Create HugCommand.js

---
+++ commands/HugCommand.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const request = require('request-promise-native');
+
+module.exports.run = async (bot, message, args, ops) => {
+
+    let options = {
+        url: 'http://api.giphy.com/v1/gifs/search',
+        qs: {
+          q: 'hug',
+          api_key: 'dc6zaTOxFJmzC',
+          limit: 10,
+          offset: 0
+        },
+        json: true
+      };
+    
+      let response = await request(options);
+    
+      if (!response.data.length) {
+        return Bastion.emit('error', Bastion.strings.error(message.guild.language, 'notFound'), Bastion.strings.error(message.guild.language, 'notFound', true, 'image'), message.channel);
+      }
+    
+      await message.channel.send({
+        embed: {
+          color: Bastion.colors.BLUE,
+          title: `A hug from ${message.author.tag}`,
+          image: {
+            url: response.data[Math.floor(Math.random() * response.data.length)].images.original.url
+          },
+          footer: {
+            text: 'Powered by GIPHY'
+          }
+        }
+      });
+}
+
+module.exports.help = {
+    name: "hug"
+  }
