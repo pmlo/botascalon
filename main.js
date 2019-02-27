301cae56d22da2b028623603baf855b90a40bd6e
ZaynMc <theobourrier@outlook.fr>
2019-02-27 20:59:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -389,7 +389,7 @@ bot.on("message", async message => {
 
       await channel.send(newChannel);
 
-      //author.send(":flag_mf: Merci d'avoir contacté le support ! Un membre du staff va vous contacter dans les plus brefs délais \n:flag_lr: Thank you for contacting the support ! A member of the staff will contact you as soon as possible");
+      author.send(":flag_mf: Merci d'avoir contacté le support ! Un membre du staff va vous contacter dans les plus brefs délais \n:flag_lr: Thank you for contacting the support ! A member of the staff will contact you as soon as possible");
 
       active.channelID = channel.id;
       active.targetID = author.id;
@@ -428,7 +428,7 @@ bot.on("message", async message => {
       message.channel.delete();
 
       db.delete(`support_${support.targetID}`);
-      message.guild.member(supportUser).ban("Test");
+      message.guild.member(supportUser).ban("Troll bot / Invite Discord");
       return;
 
     }
