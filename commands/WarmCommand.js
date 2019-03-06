738de6152da03330dc7ef07321eb2248702db8bc
ZaynMc <theobourrier@outlook.fr>
2019-03-07 00:40:55 +0100

    Update WarmCommand.js

--- commands/WarmCommand.js
+++ commands/WarmCommand.js
@@ -16,11 +16,8 @@ module.exports.run = async (bot, message, args) => {
 
     warns[wUser.id].warns++;
 
-    fs.writeFile("./warnings.json", JSON.stringify(warns) (err), {
-        if(err) {console.log(err);}
-    });
-
-
+    fs.writeFile("./warnings.json", JSON.stringify(warns));
+    
     let warnEmbed = new Discord.RichEmbed()
     .setDescription("Warns")
     .setAuthor(message.author.username)
